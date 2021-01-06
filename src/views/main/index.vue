a8f555c09e844cba0149c93312fad71222963014
JAVASM <lin837801710@163.com>
2021-01-06 14:44:03 +0800

    基础页面模板完成

--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -38,10 +38,12 @@
               <span slot="title">商品管理</span>
             </el-menu-item>
           </el-submenu>
+
           <el-menu-item index="3">
             <i class="el-icon-menu"></i>
             <span slot="title">运营商管理</span>
           </el-menu-item>
+
 
         </el-menu>
 
@@ -63,78 +65,8 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: "index"
-
-}
-</script>
-
-<style scoped lang="less">
-//表头
-.el-header, .el-footer {
-  background-color: #F56C6C;
-  color: #fff;
-  text-align: center;
-  line-height: 60px;
-}
-//滚动条
-.scrollerbar{
-  height: calc(100% - 70px);
-  width: 110%;
-}
-//下方滚动条
-//.el-aside{
-//  overflow:hidden
-//}
-
-//左测
-.el-aside {
-  background-color: #fff;
-  color: #444;
-
-  .title-wrapper {
-    line-height: 60px;
-    background-color: #F56C6C;
-    img{
-      width: 35px;
-      height: 35px;
-      border-radius: 50%;
-      vertical-align: middle;
-      margin: 0 8px 0 10px;
-    }
-    span{
-      font-weight: 700;
-      color: white;
-      font-size: 16px;
-    }
-  }
-  //导航菜单
-
-}
-
-//表内
-.el-main {
-  background-color: #E9EEF3;
-  color: #333;
-  text-align: center;
-  padding: 0;
-  overflow: hidden;
-}
-
-.el-container {
-  height: 100%;
-}
-
-.main-box {
-  height: 100%;
-  //中间样式
-  .main-body{
-    background-color: #fff;
-    height: 100%;
-    margin: 10px 0px 0px 10px;
-    padding: 20px 0px 0px 20px;
-  }
-}
-
+<!--js文件-->
+<script src="./index.js"></script>
+<!--css文件-->
+<style  lang="less"  src="./index.less">
 </style>
---
+++ src/views/supplier/index.vue
@@ -0,0 +1,84 @@
+<template>
+<div class="entity-box">
+<!--  操作的部分-->
+  <div class="execute-box">
+    <el-button-group>
+    <el-button type="primary" plain>新建</el-button>
+    <el-button type="primary" plain>批量删除</el-button>
+    </el-button-group>
+  </div>
+<!--  查询-->
+  <div class="search-box"></div>
+
+<!--  表单数据
+border 带边框
+stripe 斑马状
+tooltip-effect="dark" //表格内信息过长的话显示省略号/不换行
+
+-->
+  <div class="table-box">
+    <el-table
+        stripe
+        border
+        tooltip-effect="dark"
+        :data="tableData"
+        style="width: 100%">
+<!--      选择框-->
+      <el-table-column
+          align="center"
+      type="selection"
+      width="55"
+      ></el-table-column>
+      <el-table-column
+          align="center"
+          prop="date"
+          label="日期"
+          width="180">
+      </el-table-column>
+      <el-table-column
+          align="center"
+          prop="name"
+          label="姓名"
+          width="180">
+      </el-table-column>
+      <el-table-column
+          align="center"
+          prop="address"
+          label="地址">
+      </el-table-column>
+
+      <el-table-column
+          align="center"
+          label="操作">
+<!--        要取值得话需要加上 template-->
+        <template>
+          <el-button size="mini" type="primary">编辑</el-button>
+          <el-button size="mini" type="danger">删除</el-button>
+        </template>
+
+      </el-table-column>
+    </el-table>
+  </div>
+
+<!--  分页-->
+<div class="page-box">
+  <el-pagination
+      background
+      layout="prev, pager, next"
+      :total="1000">
+  </el-pagination>
+</div>
+
+</div>
+
+
+
+
+</template>
+
+<script src="./index.js">
+</script>
+
+<style scoped src="./index.less" lang="less">
+
+</style>
